89570566025fb9fc980fc724af36fc54e645a326
Daniel Payares <danielpayaresl@gmail.com>
2020-06-16 12:46:33 -0300

    added username to Auth

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -18,7 +18,7 @@
             <div><a href="#">www.freecodecamp.org</a></div>        
         </div>
     </div>
-    <div class="row">
+    <div class="row pt-5">
         <div class="col-4">
            <img src="https://instagram.faep11-1.fna.fbcdn.net/v/t51.2885-15/sh0.08/e35/c0.107.924.924a/s640x640/101418871_149220720095600_2797518877581952974_n.jpg?_nc_ht=instagram.faep11-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=JuVmbSr46_AAX_byzQ3&oh=7b4a5406d44db6b74fe90f81ed4b1f04&oe=5F13B14A" class="w-100">
         </div>
