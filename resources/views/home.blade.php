caae163615dccc170d4d002695e0d38d09ef536d
Daniel Payares <danielpayaresl@gmail.com>
2020-06-16 13:46:06 -0300

    created profiles table, profile model

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,14 +7,14 @@
             <img src="https://instagram.faep11-1.fna.fbcdn.net/v/t51.2885-15/e35/s150x150/101012189_621101145165772_7487155284092685876_n.jpg?_nc_ht=instagram.faep11-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=gtOeyCBtLrsAX-v4fhp&oh=d555cccbe24771e615a864ac33224738&oe=5F1305CA" alt="" class="rounded-circle">
         </div>
     <div class="col-9 pt-5">
-        <div><h1>freeCodeCamp</h1></div>
+        <div><h1>{{$user->username}}</h1></div>
             <div class="d-flex">
                 <div class="pr-4"><strong>153</strong> posts</div>
                 <div class="pr-4"><strong>20k</strong> followers</div>
                 <div class="pr-4"><strong>200</strong> following</div>
             </div>
             <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
-            <div>Mamma, uuuu, i don´t wanna die, if i´m not back again this time tomorrow carry on, carry on.. nothing really matters....</div>
+            <div>don´t stop me now, cuz i´m having a good time, having a good time...</div>
             <div><a href="#">www.freecodecamp.org</a></div>        
         </div>
     </div>
