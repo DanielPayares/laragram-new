f20733e1423602cb7630de4b8d3cadec197992fb
Daniel Payares <danielpayaresl@gmail.com>
2020-06-16 08:43:49 -0300

    CSi'm not afraid of you, CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,10 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3"></div>
-    </div>
-    <div class="col-9">
-        
+        <div class="col-3 p-5">
+            <img src="https://instagram.faep11-1.fna.fbcdn.net/v/t51.2885-15/e35/s150x150/101012189_621101145165772_7487155284092685876_n.jpg?_nc_ht=instagram.faep11-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=gtOeyCBtLrsAX-v4fhp&oh=d555cccbe24771e615a864ac33224738&oe=5F1305CA" alt="" class="rounded-circle">
+        </div>
+    <div class="col-9 pt-5">
+        <div><h1>freeCodeCamp</h1></div>
+            <div class="d-flex">
+                <div class="pr-4"><strong>153</strong> posts</div>
+                <div class="pr-4"><strong>20k</strong> followers</div>
+                <div class="pr-4"><strong>200</strong> following</div>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
