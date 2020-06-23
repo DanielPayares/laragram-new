c27f3f7e6d90c629a6ee16ba66871136ebaa5893
Daniel Payares <danielpayaresl@gmail.com>
2020-06-23 14:28:00 -0300

    worked with profile frontend

--- app/Profile.php
+++ app/Profile.php
@@ -7,6 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class Profile extends Model
 {
     protected $guarded = [];
+
+    pulic function profileImage() {
+        return '/storage/' . ($this->image) ? $this->image : 'storage/profile/7g2WRcvFT3J5EX4WaP3LWIhhDJMuutqFLrOz19uI.png'
+    }
+
     public function user() {
         return $this->belongsTo(User::class);
     }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -7,8 +7,27 @@
             <img src="/storage/{{ $post->image }}" class="w-100" alt="">
         </div>
         <div class="col-4">
-            <h3> {{ $post->user->username }}</h3>
-            <p> {{$post->caption }}</p>
+            <div class="d-flex align-items-center pb-4">
+                <div class="pl-4">
+                <img src="/storage/{{ $post->user->profile->image }}" class="rounded-circle w-50" style="max-width: 100px;">
+                </div>
+                <div class="pl-4">
+                    <div class="font-weight-bold"> 
+                        <a href="/profile/{{ $post->user->id }}">
+                            <span class="text-dark">{{ $post->user->username }} |</span>
+                        </a> 
+                        <a href="#" class="pl-4">Follow</a>
+                    </div>
+                </div>
+            </div>
+            <hr>
+            <p> 
+                <span class="font-weight-bold"> 
+                    <a href="/profile/{{ $post->user->id }}">
+                        <span class="text-dark">{{ $post->user->username }}</span>
+                    </a> 
+                </span>{{$post->caption }}</span>
+            </p>
         </div>
     </div>
 </div>
