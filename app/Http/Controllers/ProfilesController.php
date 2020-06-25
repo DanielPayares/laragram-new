937bf2579d6a2bbe3bc13326866c2a4290ac5830
Daniel Payares <danielpayaresl@gmail.com>
2020-06-25 20:11:46 -0300

    worked with cache and optimization

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -4,14 +4,39 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Intervention\Image\Facades\Image;
 
 class ProfilesController extends Controller
 {
     public function index(User $user) {
         $follows = (auth()->user()) ? auth()->user()->following->contains($user->id) : false;
+        $postCount = Cache::remember(
+            'count.posts.' . $user->id, 
+            now()->addSeconds(30), 
+            function() use ($user) {
+                return $user->posts()->count();
+        });
 
-        return view('profiles.index', compact('user'));
+        $followersCount = Cache::remember(
+            'count.followers.' . $user->id,
+            now()->addSeconds(30),
+            function() use ($user) {
+                return $user->profile->followers->count();
+            });
+        
+        
+        $followingCount = Cache::remember(
+            'count.following.' . $user->id,
+            now()->addSeconds(30),
+            function () use ($user) {
+                return $user->profile->followers->count();
+            });
+        
+        
+        
+        $user->following->count();
+        return view('profiles.index', compact('user', 'postCount', 'followersCount', 'followingCount'));
     }
 
     public function edit(User $user)
